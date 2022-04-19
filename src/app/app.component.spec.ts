e674ab5e696cab1ba5875ab81245416ad85eb5e8
jm <biaggi>
2022-04-19 22:43:40 +0200

    chore(): added more tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         RouterTestingModule
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   });
+@Component({ selector: 'app-navbar', template: '' })
+class NavbarStubComponent {}
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, NavbarStubComponent],
+    }).compileComponents();
 
-//   it(`should have as title 'angular-http-effects'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('angular-http-effects');
-//   });
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('.content span')?.textContent).toContain('angular-http-effects app is running!');
-//   });
-// });
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'angular-http-effects'`, () => {
+    expect(app.title).toEqual('angular-http-effects');
+  });
+
+  it('should render title', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('nav')).toBeDefined();
+  });
+});
--- src/app/usuarios/lista/lista.component.spec.ts
+++ src/app/usuarios/lista/lista.component.spec.ts
@@ -1,25 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListaComponent } from './lista.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState, initialState } from 'src/app/store/store';
+import { initialStateUsers } from 'src/app/store/reducers';
+import * as actions from '../../store/actions';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// describe('ListaComponent', () => {
-//   let component: ListaComponent;
-//   let fixture: ComponentFixture<ListaComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ ListaComponent ]
-//     })
-//     .compileComponents();
-//   });
+describe('ListaComponent', () => {
+  let component: ListaComponent;
+  let fixture: ComponentFixture<ListaComponent>;
+  let store: MockStore<AppState>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ListaComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  const initialStateMock: AppState = {
+    ...initialState,
+    users: {
+      ...initialStateUsers,
+      users: [{ id: 1, first_name: 'a', last_name: 'b', avatar: 'c' }],
+    },
+  };
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  const userService = jasmine.createSpyObj('userService', ['getUser']);
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ListaComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+    fixture = TestBed.createComponent(ListaComponent);
+    component = fixture.componentInstance;
+    store.setState(initialStateMock);
+
+    spyOn(store, 'dispatch').and.callFake(() => {});
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should dispatch action to get data', () => {
+    expect(store.dispatch).toHaveBeenCalledWith(actions.cargarUsuarios());
+  });
+
+  it('should render a user', () => {
+    const element: DebugElement = fixture.debugElement.query(By.css('.card-title'));
+    expect(element.nativeElement.textContent).toBe('a');
+  });
+});
